c53d58ea5cadf20be1e32ff53f32ba2886944553
Geet <geet1998patel@gmail.com>
2019-08-22 10:05:17 +0530

    minor update

--- lab1/run.sh
+++ lab1/run.sh
@@ -1,5 +1,6 @@
 echo "basic cleanup.."
 rm output
+rm vector_triad
 gcc vector_triad_arr.c -o vector_triad
 echo "vector_triad_arr.c compiled.."
 ./vector_triad
@@ -7,5 +8,6 @@ echo "vector_triad calculations done and output file generated...."
 echo "generating plot.."
 python3 performance_plot.py
 echo "cleanup.."
+rm output
 rm vector_triad
 echo "cleanup done, now exiting"
